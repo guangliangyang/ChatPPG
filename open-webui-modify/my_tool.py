527341f0d50ddc13349e0fc2d2b21c2cdce79055
ricky <guangliang.yang@hotmail.com>
2024-11-28 21:44:29 +1300

    update prompt

--- open-webui-modify/my_tool.py
+++ open-webui-modify/my_tool.py
@@ -1,6 +1,70 @@
 import os
 import requests
 from datetime import datetime
+import json
+
+performance_data_json = {
+    "Speed Statistics": {
+        "Depth": {"Max (km/h)": 0.06, "Average (km/h)": 0.02},
+        "Forward": {"Max (km/h)": 0.02, "Average (km/h)": 0.01},
+        "Overall": {"Max (km/h)": 0.06, "Average (km/h)": 0.03},
+        "Sideways": {"Max (km/h)": 0.03, "Average (km/h)": 0.01},
+    },
+    "Calories Burned": {
+        "Total Exercise Duration": "00:00:48",
+        "Total Calories Burned (kcal)": 3.7,
+        "Average Calories Burned per Hour (kcal)": 276.5,
+        "Intensity": "Entertainment",
+    },
+    "Covered Area (m²)": 2.25,
+    "Cell Ratios (%)": {
+        "R22": 0.0, "R12": 0.0, "R02": 0.0, "R21": 0.0, "R11": 0.0, "R01": 0.0,
+        "R20": 0.0, "R10": 0.0, "R00": 0.0, "L20": 0.0, "L10": 0.0, "L00": 0.0,
+        "L21": 0.0, "L11": 17.2, "L01": 82.7, "L22": 0.1, "L12": 0.0, "L02": 0.0,
+    },
+    "Stroke": {
+        "Total Count": 62,
+        "Templates": {
+            "Forehand-Backspin": {"Count": 3, "Percentage (%)": 4.8},
+            "Forehand-Topspin": {"Count": 0, "Percentage (%)": 0.0},
+            "Backhand-Backspin": {"Count": 4, "Percentage (%)": 6.5},
+            "Backhand-Topspin": {"Count": 55, "Percentage (%)": 88.7},
+        },
+    },
+    "Footwork": {
+        "Total Count": 133,
+        "Templates": {
+            "Little-Step": {"Count": 66, "Percentage (%)": 49.6},
+            "Striding-Step": {"Count": 67, "Percentage (%)": 50.4},
+            "Cross-Step": {"Count": 0, "Percentage (%)": 0.0},
+        },
+    },
+}
+
+foul_data_json1 = {
+    "Foul Serves/Total Serve Actions": {"Foul Serves": 4, "Total Serve Actions": 20},
+    "Foul Statistics": {
+        "In Front of the End Line": 2,
+        "Beyond the Sideline Extension": 0,
+        "Tossed from Below Table Surface": 0,
+        "Backward Angle More Than 30°": 3,
+        "Tossed Upward Less Than 16 cm": 2,
+    },
+}
+
+foul_data_json = {
+    "Summary of Serves": {
+        "Total Serves": 20,
+        "Foul Serves": 4
+    },
+    "Details of Foul Statistics": {
+        "Serves in Front of the End Line": 2,
+        "Serves Beyond the Sideline Extension": 0,
+        "Serves Tossed from Below the Table Surface": 0,
+        "Serves with Backward Angle Greater Than 30 Degrees": 3,
+        "Serves Tossed Upward Less Than 16 cm": 2
+    }
+}
 
 
 
@@ -8,21 +72,14 @@ class Tools:
     def __init__(self):
         pass
 
-    # Add your custom tools using pure Python code here, make sure to add type hints
-    # Use Sphinx-style docstrings to document your tools, they will be used for generating tools specifications
-    # Please refer to function_calling_filter_pipeline.py file from pipelines project for an example
-
-    def analy_table_tennis_perfomance(self, file_path: str) -> str:
-        print("file_path:", str)
-        result = " Speed Statistics : 3m per second ,Calories Burned:400 kcal, Average Calories Burned per Hour: 600 kcal,   Covered Area: 15 m square - Swing Count: 200 - Footwork: 4000"
-
+    def analy_table_tennis_performance(self, file: str) -> str:
+        print("analy_table_tennis_performance,video_file_path:", file)
+        result = json.dumps(performance_data_json)
+        print(result)
         return result
 
-    def detect_serve_foul(self, file_path: str):
-        result = ("Tossed from Below Table Surface:1 time, occur at 1:50; "
-                  "In Front of the End Line: 1 time, occur at 2:50; "
-                  "Beyond the sideline extension 1 time, occur at 3:50; "
-                  "Backward Angle More Than 30°: 1 time, occur at 4:50; "
-                  "Tossed Upward Less Than 16 cm: 0 time")
-
+    def detect_player_foul(self, file: str):
+        print("detect_serve_foul,video_file_path:", file)
+        result = json.dumps(foul_data_json)
+        print(result)
         return result
--- open-webui-modify/readme.txt
+++ open-webui-modify/readme.txt
@@ -2,3 +2,45 @@
 1. open-webui\backend\open_webui\apps\retrieval\loaders\main.py
 
 2. open-webui-pipeline\my_pipeline.py
+
+
+
+3. model prompt:
+
+
+--------------------------------------------------------------------------
+ You will get a picture from the user or ask a user politely for a picture  to analysis. Once you get the picture,  user may ask you 2 kinds of question:
+1. analysis player performance(analy_table_tennis_perfomance in  provided tool )
+2. detect serve foul (detect_serve_foul in  provided tool )
+all of these user request, you MUST use a provided tool to calculate.
+You answer to the user DIRECTLY as the tool answers, not from yourself
+
+
+--------------------------------------------------------------------------
+ You will get a file from the user or ask a user politely for a picture  to analysis. Once you get the file,  user may ask you 2 kinds of question:
+1. analysis player performance(analy_table_tennis_performance in  provided tool )
+2. detect player foul (detect_player_foul in  provided tool )
+all of these user request, you MUST use a provided tool to  perform the computations.
+You answer to the user DIRECTLY as the tool answers, not from yourself, not combine the old context, only the latest answer from provided tool
+
+--------------------------------------------------------------------------
+    You will either receive an mp4 video file from the user or politely request the user to provide the video for analysis if not already provided. The user may ask questions related to the following two topics:
+
+    1. Analyzing player performance: Use the tool analy_table_tennis_performance.
+    2. Detecting serve fouls: Use the tool detect_serve_foul.
+
+    For these user requests, follow these strict requirements:
+
+    1. Use the provided tools to perform the computations. If the relevant function is not available in the tools, respond politely: "I couldn't find an appropriate CV model to analyze the video."
+
+    2. When you obtain results from the tools, follow these 3 steps to present the response:
+
+        2.1 Display the raw JSON data: Present the original JSON data returned by the tool without any modifications or alterations.
+        2.2 Create a table or chart: Organize and visualize the JSON data into a clear and logical table or chart to enhance user understanding.
+        2.3 Provide a video preview: Include an interactive preview of the mp4 video in your response, such as a thumbnail with a play button or another form of video preview.
+
+    Always adhere to these steps when responding to the user's queries and avoid adding any additional explanations or interpretations beyond the user's requests.
+
+--------------------------------------------------------------------------
+4. please analysis the player performance in the video
+5. please detect player foul  in the video
